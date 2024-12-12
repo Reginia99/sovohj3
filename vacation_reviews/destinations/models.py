eecab9771829e856e9539fe54fb4637940566d12
Kahibursa <arttu.kahilainen@gmail.com>
2024-12-12 16:09:32 +0200

    Update models.py

--- vacation_reviews/destinations/models.py
+++ vacation_reviews/destinations/models.py
@@ -24,3 +24,6 @@ class Review(models.Model):
 class Profile(models.Model):
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='profile_pics/')
+
+     def __str__(self):
+        return self.name
